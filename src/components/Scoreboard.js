73fdc7b4d575c081320739907de77db63752d2c6
gbeason <trey.beason@yahoo.com>
2022-02-10 16:32:55 -0600

    ScoreRows now displayed in Scoreboard

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AboutScoreboard from './components/AboutScoreboard';
+import ScoreRows from './components/ScoreRows';
+import { useTable } from 'react-table';
 
 const Scoreboard = () => {
     return (
         <div className="scoreboard">
-            <AboutScoreboard/>
+            <AboutScoreboard />
+            <ScoreRows />
         </div>
     );
 }
