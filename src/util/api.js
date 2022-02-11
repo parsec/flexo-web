2bc4b0c8306e200a2bfbc94fbd4bb07961d0cf0c
parsec <beckmanseth@gmail.com>
2022-02-10 22:54:27 -0500

    feat(api): added fetch APIs

---
+++ src/util/api.js
@@ -0,0 +1,101 @@
+export async function getTeams(secret) {
+    let response = await fetch(`https://dev-app.flexo.wtf/teams`, {
+        "method": "GET",
+        "headers": {
+            "Authorization": "Bearer " + secret
+        }
+    });
+
+    if (response.ok) {
+        let json = await response.json();
+        console.log(json)
+        return json;
+    } else {
+        alert("HTTP-Error: " + response.status);
+    }
+}
+
+export async function getCategories(secret) {
+    let response = await fetch(`https://dev-app.flexo.wtf/categories`, {
+        "method": "GET",
+        "headers": {
+            "Authorization": "Bearer " + secret
+        }
+    });
+
+    if (response.ok) {
+        let json = await response.json();
+        console.log(json)
+        return json;
+    } else {
+        alert("HTTP-Error: " + response.status);
+    }
+}
+
+export async function getEvents(secret) {
+    let response = await fetch(`https://dev-app.flexo.wtf/events`, {
+        "method": "GET",
+        "headers": {
+            "Authorization": "Bearer " + secret
+        }
+    });
+
+    if (response.ok) {
+        let json = await response.json();
+        console.log(json)
+        return json;
+    } else {
+        alert("HTTP-Error: " + response.status);
+    }
+}
+
+export async function getTargets(secret) {
+    let response = await fetch(`https://dev-app.flexo.wtf/targets`, {
+        "method": "GET",
+        "headers": {
+            "Authorization": "Bearer " + secret
+        }
+    });
+
+    if (response.ok) {
+        let json = await response.json();
+        console.log(json)
+        return json;
+    } else {
+        alert("HTTP-Error: " + response.status);
+    }
+}
+
+export async function getAllTeamsReport(secret) {
+    let response = await fetch(`https://dev-app.flexo.wtf/report/teams`, {
+        "method": "GET",
+        "headers": {
+            "Authorization": "Bearer " + secret
+        }
+    });
+
+    if (response.ok) {
+        let json = await response.json();
+        console.log(json)
+        return json;
+    } else {
+        alert("HTTP-Error: " + response.status);
+    }
+}
+
+export async function getTeamReport(secret, teamID) {
+    let response = await fetch(`https://dev-app.flexo.wtf/report/team/${teamID}`, {
+        "method": "GET",
+        "headers": {
+            "Authorization": "Bearer " + secret
+        }
+    });
+
+    if (response.ok) {
+        let json = await response.json();
+        console.log(json)
+        return json;
+    } else {
+        alert("HTTP-Error: " + response.status);
+    }
+}
